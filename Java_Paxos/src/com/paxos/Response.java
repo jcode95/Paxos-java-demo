296a4bcaba52c23cc2d9f458ad99f53849d8ff32
ZJ <32047744+jcode95@users.noreply.github.com>
2020-06-24 20:14:48 +0800

    初始化提交
    paxos的java  demo，有兴趣的可以下载来试试

---
+++ Java_Paxos/src/com/paxos/Response.java
@@ -0,0 +1,14 @@
+package com.paxos;
+
+/**
+ * @author jiezhou
+ * @CalssName: Response
+ * @Package com.paxos
+ * @Description: 响应pok
+ * @date 2020/6/24/17:40
+ */
+public class Response {
+
+
+
+}
